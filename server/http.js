26d5e5fd73045831c6fd4df9c1931fa8031eec10
liutaochange <liutaochange@163.com>
2018-06-23 18:21:30 +0800

    await

--- server/http.js
+++ server/http.js
@@ -1,6 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
+// 中间件
+const middle = async (ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    await next()
+}
+const middle1 = async (ctx, next) => {
+    ctx.body = 'hi'
+    await next()
+}
+const middle2 = async (ctx, next) => {
+    ctx.body = ctx.body + 'Luke'
+}
 app.use(async (ctx, next) => {
     ctx.body = 'hello koa'
 });
 app.listen(3000);
+
